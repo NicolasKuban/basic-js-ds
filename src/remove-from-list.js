018da56442b919b19fedec167ef3273df640c076
NicolasKuban <nicrad@yandex.ru>
2023-12-14 00:54:21 +0300

    feat: Приступил к выполнению ListRemove

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,10 +22,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = new ListNode()
+  let i = 0
+  while (l) {
+    i += 1
+    if (i > 10) {break}
+    if (l.value === k) {
+      console.log("need delete")
+      l = l.next
+      continue
+    }
+    result.value = l.value
+    l = l.next
+  }
+  return l
 }
+
+
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+
+a = convertArrayToList([3, 1, 2, 3, 4, 5])
+console.log(removeKFromList(a, 3))
 
 module.exports = {
   removeKFromList
