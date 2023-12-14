095beb292852d7b674bcce4aa44a32143c0d5afe
NicolasKuban <nicrad@yandex.ru>
2023-12-15 00:30:47 +0300

    feat: Реализовано решение задачи ListRemove

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,23 +22,89 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
+
+
+
+
+
 function removeKFromList(l, k) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  let result = new ListNode()
+  let result = null
   let i = 0
+  let last
   while (l) {
     i += 1
+    console.log("Step", i, "-", l.value)
     if (i > 10) {break}
+    
     if (l.value === k) {
       console.log("need delete")
+      console.log("===До", l, l.next)
+      if (l.next === null) {
+        console.log(result)
+        console.log(last)
+        last.next = null
+        console.log(last)
+        break
+      }
       l = l.next
+      if (l !== null) {
+        console.log("===После", l, l.next )
+      } 
+      // else {
+      //   last = null
+      //   break
+      // }
       continue
     }
-    result.value = l.value
+    // console.log(result.next)
+    if (result === null) {
+      result = l
+      console.log("first element", result)
+    //   console.log("first", result)
+      last = l
+      console.log("last", last)
+    } else {
+      console.log("last===До", last, last.next)
+      last.next = l
+      last = last.next
+      console.log("last===После", last, last.next)
+      console.log("----------------")
+    }
+    console.log("===До", l, l.next)
     l = l.next
+    if (l !== null) {
+      console.log("===После", l, l.next )
+    }
+    
+    console.log("==========")
+      
+
+  //   else {
+  //     console.log("===Last до", last, l)
+  //     last.next = l
+  //     last = last.next
+  //     console.log("===Last после", last, l)
+  //     console.log("===До", l, l.next)
+  //     l = l.next
+  //     if (l !== null) {
+  //       console.log("===После", l, l.next )
+  //     }
+  //     console.log("decrease", l)
+  //     console.log("----------")
+  //   //   last = last.next
+  //   //   console.log("last", last)
+  //   //   console.log("===До", l, l.next)
+  //   //   l = l.next
+  //   //   console.log("===После", l, l.next)
+  //   //   console.log("decrease", l)
+  //   //   console.log("----------")
+  // }
+    
+
   }
-  return l
+  return result
 }
 
 
@@ -48,6 +114,9 @@ class ListNode {
     this.next = null;
   }
 }
+
+
+
 
 function convertArrayToList(arr) {
   return arr.reverse().reduce((acc, cur) => {
@@ -61,8 +130,19 @@ function convertArrayToList(arr) {
   }, null);
 }
 
-a = convertArrayToList([3, 1, 2, 3, 4, 5])
-console.log(removeKFromList(a, 3))
+a = convertArrayToList([3, 1, 2, 3])
+b = removeKFromList(a, 3)
+// console.log(b)
+
+console.log("=========")
+i = 0
+while (b) {
+  console.log("Step", i, "-", b.value)
+  b = b.next
+  i++
+  if (i>10) {break}
+}
+console.log("=========")
 
 module.exports = {
   removeKFromList
